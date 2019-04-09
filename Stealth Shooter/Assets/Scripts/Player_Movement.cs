4707592139cfc29c686f898e685a3b07c9752c1d
Gablerkeira <kgabler2@stumail.jccc.edu>
2019-04-09 16:49:20 -0500

    Basic Character Controller(for now)

--- Stealth Shooter/Assets/Scripts/Player_Movement.cs
+++ Stealth Shooter/Assets/Scripts/Player_Movement.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float walkSpeed;
+    public float crouchSpeed;
+    public float rotateSpeed;
+    Vector3 totalForce = Vector3.zero;
+    public Rigidbody rb;
+    public Animator playerAnimator;
+    public Action OnFire = delegate { };
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        totalForce = Vector3.zero;
+
+        totalForce += transform.right * Input.GetAxis("Horizontal") * walkSpeed;
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            totalForce += transform.forward * Input.GetAxis("Vertical") * walkSpeed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            totalForce += transform.forward * Input.GetAxis("Vertical") * walkSpeed;
+        }
+
+        transform.position += totalForce * Time.deltaTime;
+        //playerAnimator.SetFloat("Velocity", totalForce.magnitude);
+
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rotateSpeed);
     }
 }
