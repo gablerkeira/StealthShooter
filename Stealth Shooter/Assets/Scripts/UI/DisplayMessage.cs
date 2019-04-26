b39003b07c1904a8bba9703bf542522774b72299
DominiQue Johnson <Dominique.johnson14@yahoo.com>
2019-04-26 14:49:18 -0500

    Created a UI script that displays messages when the player enters a field Created Nav mesh for Warehouse Level

---
+++ Stealth Shooter/Assets/Scripts/UI/DisplayMessage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayMessage : MonoBehaviour
+{
+    public Text Message;
+    public Image Panel;
+
+    private void Start()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        Message.GetComponent<Text>().enabled = false;
+        Panel.GetComponent<Image>().enabled = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Message.GetComponent<Text>().enabled = true;
+        Panel.GetComponent<Image>().enabled = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Message.GetComponent<Text>().enabled = false;
+        Panel.GetComponent<Image>().enabled = false;
+    }
+}
