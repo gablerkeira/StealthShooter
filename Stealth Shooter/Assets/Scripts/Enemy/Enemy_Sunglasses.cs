63c8f321b72a54f6f8ef3b5743dab410f37fdff2
Keira gabler <kgabler2@stumail.jccc.edu>
2019-05-13 14:50:53 -0500

    Tried sunglasses

---
+++ Stealth Shooter/Assets/Scripts/Enemy/Enemy_Sunglasses.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Sunglasses : MonoBehaviour
+{
+    public Material sunglasses;
+    public List<GameObject> enemies = new List<GameObject>();
+
+    private void Start()
+    {
+        foreach(GameObject enemy in enemies)
+        {
+            //enemies.Add(GameObject.FindGameObjectWithTag());
+        }
+    }
+}
