acb155918b4c6f3622e1178da884f59b5ee4596f
DominiQue Johnson <Dominique.johnson14@yahoo.com>
2019-05-17 14:10:24 -0500

    Added audio stuff to the particle test script

--- Stealth Shooter/Assets/ParticleTest.cs
+++ Stealth Shooter/Assets/ParticleTest.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class ParticleTest : MonoBehaviour
 {
     public ParticleSystem Soundgun;
+    public AudioClip FiringGun;
+    public AudioSource ShootGun;
 
+    private void Start()
+    {
+        ShootGun = GetComponent<AudioSource>();
+    }
     private void OnMouseDown()
     {
         Soundgun.Play();
+        ShootGun.Play();
+
+        print("We got noise!");
     }
 }
