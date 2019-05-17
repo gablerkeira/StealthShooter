7f7a046a4f3321ce091f04ffcbcaaffee586197d
DominiQue Johnson <Dominique.johnson14@yahoo.com>
2019-05-17 13:54:00 -0500

    Made a test script for the particle system. Fixed messages in my level. Took Triston off credits (conflic creator)

---
+++ Stealth Shooter/Assets/ParticleTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTest : MonoBehaviour
+{
+    public ParticleSystem Soundgun;
+
+    private void OnMouseDown()
+    {
+        Soundgun.Play();
+    }
+}
